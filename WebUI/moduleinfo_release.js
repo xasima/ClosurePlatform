c5c08c9ce8088180bb2f3646bb666c153b797e3b
xasima <xasima@gmail.com>
2013-04-19 20:14:01 +0300

    Changes path to moduleinfo_release due to gitignored js/modules. Some fixes of index_.html

---
+++ WebUI/moduleinfo_release.js
@@ -0,0 +1,12 @@
+var MODULE_INFO = {
+	'closure':[],
+	'template_theme':["closure"],
+	'commons':["closure","template_theme"],
+	'app':["commons"],'settings':["app"]};
+
+var MODULE_URIS = {
+	'closure':['js/module/default/ru/closure.js'],
+	'template_theme':['js/module/default/ru/template_theme.js'],
+	'commons':['js/module/default/ru/commons.js'],
+	'app':['js/module/default/ru/app.js'],
+	'settings':['js/module/default/ru/settings.js']};
